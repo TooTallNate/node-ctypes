30c3daefcd853e46b41011d9bdae55ca9243c72d
Nathan Rajlich <nathan@tootallnate.net>
2015-09-28 11:57:33 -0700

    progress point...

---
+++ index.js
@@ -0,0 +1,58 @@
+/**
+ * https://developer.mozilla.org/en-US/docs/Mozilla/js-ctypes/js-ctypes_reference/ctypes
+ */
+
+var ffi = require('ffi');
+var Library = require('./lib/Library');
+
+exports.cast = cast;
+exports.libraryName = libraryName;
+exports.open = open;
+
+/**
+ * ABI:
+ * https://developer.mozilla.org/en-US/docs/Mozilla/js-ctypes/js-ctypes_reference/ABI
+ *
+ * Ported from:
+ * https://dxr.mozilla.org/mozilla-central/source/js/src/ctypes/CTypes.cpp#6091
+ */
+
+exports.default_abi = ffi.FFI_DEFAULT_ABI;
+exports.stdcall_abi = exports.winapi_abi = (function () {
+  if (process.platform === 'win32') {
+    if (process.arch === 'x64') {
+      return ffi.FFI_WIN64;
+    } else {
+      return ffi.FFI_STDCALL;
+    }
+  } else {
+    return ffi.FFI_DEFAULT_ABI;
+  }
+})();
+
+/**
+ * https://developer.mozilla.org/en-US/docs/Mozilla/js-ctypes/js-ctypes_reference/ctypes#cast%28%29
+ */
+
+function cast (data, type) {
+}
+
+/**
+ * Returns the correct platform-specific filename for a given library name (e.g.
+ * libnss3.dylib for nss3 on OS X.)
+ *
+ * https://developer.mozilla.org/en-US/docs/Mozilla/js-ctypes/js-ctypes_reference/ctypes#libraryName%28%29
+ * https://dxr.mozilla.org/mozilla-central/source/js/src/ctypes/Library.cpp#51-81
+ */
+
+function libraryName (name) {
+  return 'lib' + name + ffi.LIB_EXT;
+}
+
+/**
+ * https://developer.mozilla.org/en-US/docs/Mozilla/js-ctypes/js-ctypes_reference/ctypes#open()
+ */
+
+function open (libSpec) {
+  return new Library(libSpec);
+}
