8500f30072172a11d0548836cddbff2dd2849959
Nathan Rajlich <nathan@tootallnate.net>
2015-12-03 14:23:05 -0800

    test: use `exports` mocha UI
    Easier to map back to an object

--- test/test_mocha.js
+++ test/test_mocha.js
@@ -1,21 +1,26 @@
 var assert = require('assert');
 var tests = require('./test-jpm');
 
-describe('ctypes', function () {
+Object.keys(tests).forEach(function (name) {
+  var parts = name.replace('test ', '').split('#');
+  var title = parts.pop();
 
-  Object.keys(tests).forEach(function (name) {
+  var curr = exports;
+  while (parts.length) {
+    var next = parts.shift();
+    if (!curr[next]) curr[next] = {};
+    curr = curr[next];
+  }
 
-    it(name, function (done) {
-      var test = tests[name];
-      var async = test.length === 2;
-      if (async) {
-        test(assert, done);
-      } else {
-        test(assert);
-        done();
-      }
-    });
-
-  });
+  curr[title] = function (done) {
+    var test = tests[name];
+    var async = test.length === 2;
+    if (async) {
+      test(assert, done);
+    } else {
+      test(assert);
+      done();
+    }
+  };
 
 });
