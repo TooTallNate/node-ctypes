26de5791c9b04d46c7abe4df2954b83f064dd9df
Nathan Rajlich <nathan@tootallnate.net>
2015-12-09 13:07:45 -0800

    more tests

--- test/test-jpm.js
+++ test/test-jpm.js
@@ -10,8 +10,44 @@ if (isNode) {
 }
 
 
-exports['test main'] = function(assert) {
-  assert.equal(ctypes.char.toString(), 'type char');
+exports['test CData#throws TypeError for out of range'] = function(assert) {
+  var err;
+  try {
+    ctypes.uint8_t(-1);
+  } catch (e) {
+    err = e;
+  }
+
+  assert.ok(!!err);
+  assert.equal(err.message, 'can\'t convert the number -1 to the type uint8_t');
+
+  err = null;
+  try {
+    ctypes.uint8_t('a');
+  } catch (e) {
+    err = e;
+  }
+
+  assert.ok(!!err);
+  assert.equal(err.message, 'can\'t convert the string "a" to the type uint8_t');
+};
+
+exports['test CData#value'] = function(assert) {
+  assert.equal(ctypes.uint8_t(1).value, 1);
+  assert.equal(ctypes.uint8_t(255).value, 255);
+};
+
+
+exports['test CType#name'] = function(assert) {
+  assert.equal(ctypes.uint8_t.name, 'uint8_t');
+  assert.equal(ctypes.char.name, 'char');
+};
+
+exports['test CType#size'] = function(assert) {
+  assert.equal(ctypes.uint8_t.size, 1);
+  assert.equal(ctypes.uint16_t.size, 2);
+  assert.equal(ctypes.uint32_t.size, 4);
+  assert.equal(ctypes.uint64_t.size, 8);
 };
 
 
