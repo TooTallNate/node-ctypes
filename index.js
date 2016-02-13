5ad1b4965eb13fe4056d5c0d8c1d0a8f5480ac0c
Nathan Rajlich <nathan@tootallnate.net>
2016-02-12 21:12:36 -0800

    index: remove unused `ctypes` var

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 if (typeof process == 'object') {
   // nodejs
   // invoke `node t` in the root of this module to run `t.js` test script
-  var ctypes = module.exports = require('./lib/index');
+  module.exports = require('./lib/index');
 } else {
   // jpm
   // invoke `jpm run` in the root of this module to run `t.js` test script
